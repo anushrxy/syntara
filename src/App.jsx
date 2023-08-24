44e3b3f46cfd2f48788962db6e9697f43bee4a4f
SirSimon162 <anmolpattnayak2016@gmail.com>
2023-08-24 19:30:17 +0530

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// src/App.jsx
+import React from 'react';
+import "./index.css";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Syntara</h1>
+    </div>
+  );
+};
+
+export default App;
